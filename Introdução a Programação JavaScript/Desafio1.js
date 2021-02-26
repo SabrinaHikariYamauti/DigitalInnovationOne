ada538cbfe7b699d64ab46e54e51b718c4aa86b2
SabrinaHikariYamauti <hikarijpfriends@hotmail.com>
2021-02-26 16:06:07 -0300

    desafiofim

--- Introdução a Programação JavaScript/Desafio1.js
+++ Introdução a Programação JavaScript/Desafio1.js
@@ -1,5 +1,12 @@
-let line = gets().split(" ");
+let line = gets().split(" "); // retorna  aproxima linha de entrada
 let A = parseInt(line[0]);
 let B = parseInt(line[1]);
 let total = 0; // Altere o valor da variável com o cálculo esperado
-console.log("X = " + total);
+console.log("X = " + total); //print na tela
+
+let valor1 = parseInt(gets());//numero de um colaborador
+let valor2 = parseInt(gets());//numero de horas trabalhadas
+let valor3 = parseFloat(gets());//valor que recebe por hora
+let salary = parseFloat(valor2 * valor3).toFixed(2); // Digite aqui o calculo do salário
+console.log("NUMBER = " + valor1);
+console.log("SALARY = U$ " + salary);

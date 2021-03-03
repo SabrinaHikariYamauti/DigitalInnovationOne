eee960c3f2c94f8b958856fb5d6d3bb3a58258bc
SabrinaHikariYamauti <hikarijpfriends@hotmail.com>
2021-03-02 23:52:56 -0300

    cobrinha se mexendo

--- Introdução a Programação JavaScript/Cobrinha/cobrinha.js
+++ Introdução a Programação JavaScript/Cobrinha/cobrinha.js
@@ -6,6 +6,10 @@ snake[0]={
     x:8*box,
     y:8*box
 }
+
+let direction="right";
+
+
 function criarRGB(){
     context.fillStyle="lightgreen";
     context.fillRect(0,0, 16*box,16*box);/*desnenha onde quadrado*/
@@ -18,5 +22,41 @@ function criarCobrinha(){
     }
 }
 
-criarRGB();
-criarCobrinha();
+document.addEventListener('keydown', update);
+
+function update(event){
+    if(event.keyCode==37 && direction!="right") direction="left";
+    if(event.keyCode==38 && direction!="down") direction="up";
+    if(event.keyCode==39 && direction!="left") direction="right";
+    if(event.keyCode==40 && direction!="up") direction="down";
+}
+
+function iniciarJogo(){
+
+    if(snake[0].x>15*box&&direction=="right") snake[0].x=0;
+    if(snake[0].x<0*box&&direction=="left") snake[0].x=16*box;
+    if(snake[0].y>15*box&&direction=="down") snake[0].y=0;
+    if(snake[0].y<0*box&&direction=="up") snake[0].y=16*box; 
+
+    criarRGB();
+    criarCobrinha();
+
+    let snakeX= snake[0].x;
+    let snakeY= snake[0].y;
+
+    if(direction=="right") snakeX+=box;
+    if(direction=="left") snakeX-=box;
+    if(direction=="up") snakeY-=box;
+    if(direction=="down") snake+=box;
+
+    snake.pop();
+
+    let newHead={
+        x:  snakeX,
+        y:  snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let jogo=setInterval(iniciarJogo,100);

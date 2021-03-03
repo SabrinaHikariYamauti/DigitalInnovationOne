18beccc9dc1be21d9950cb41696b79f9aee6e37e
SabrinaHikariYamauti <hikarijpfriends@hotmail.com>
2021-03-03 00:17:03 -0300

    cobrinhacomendo

--- Introdução a Programação JavaScript/Cobrinha/cobrinha.js
+++ Introdução a Programação JavaScript/Cobrinha/cobrinha.js
@@ -8,7 +8,10 @@ snake[0]={
 }
 
 let direction="right";
-
+let food={
+    x:Math.floor(Math.random()*15+1)*box,
+    y:Math.floor(Math.random()*15+1)*box
+}
 
 function criarRGB(){
     context.fillStyle="lightgreen";
@@ -20,6 +23,11 @@ function criarCobrinha(){
         context.fillStyle="green";
         context.fillRect(snake[i].x, snake[i].y, box,box);
     }
+}
+
+function drawFood(){
+    context.fillStyle="red";
+    context.fillRect(food.x,food.y,box,box);
 }
 
 document.addEventListener('keydown', update);
@@ -34,12 +42,13 @@ function update(event){
 function iniciarJogo(){
 
     if(snake[0].x>15*box&&direction=="right") snake[0].x=0;
-    if(snake[0].x<0*box&&direction=="left") snake[0].x=16*box;
+    if(snake[0].x<0&&direction=="left") snake[0].x=16*box;
     if(snake[0].y>15*box&&direction=="down") snake[0].y=0;
-    if(snake[0].y<0*box&&direction=="up") snake[0].y=16*box; 
+    if(snake[0].y<0 &&direction=="up") snake[0].y=16*box; 
 
     criarRGB();
     criarCobrinha();
+    drawFood();
 
     let snakeX= snake[0].x;
     let snakeY= snake[0].y;
@@ -47,9 +56,14 @@ function iniciarJogo(){
     if(direction=="right") snakeX+=box;
     if(direction=="left") snakeX-=box;
     if(direction=="up") snakeY-=box;
-    if(direction=="down") snake+=box;
+    if(direction=="down") snakeY+=box;
 
-    snake.pop();
+    if(snakeX!=food.x||snakeY!=food.y){
+        snake.pop();
+    }else{
+        food.x=Math.floor(Math.random()*15+1)*box;
+        food.y=Math.floor(Math.random()*15+1)*box;
+    }
 
     let newHead={
         x:  snakeX,
